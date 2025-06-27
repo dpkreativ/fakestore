650aed4040dfa97e74c1e0e9e50e7418af775844
Divine Orji <dpkreativ@gmail.com>
2025-06-27 11:58:03 +0100

    feature: header and button components create

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <>
+      <Header />
       <h1>Fake Store</h1>
       <h2>This is a fake store</h2>
     </>
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,3 +1,16 @@
+import Button from "./button/button";
+import style from "./header.module.css";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className={style.header}>
+      <div className={style.logo}>Fake Store Ltd.</div>
+
+      <nav>
+        <a href="/">Home</a>
+        <a href="/products">Products</a>
+        <Button />
+      </nav>
+    </header>
+  );
 }
